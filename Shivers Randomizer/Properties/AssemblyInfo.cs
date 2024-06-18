e77d62d2f442b620fb1a3d77ef8aba1752747ae5
Kory Dondzila <korydondzila@gmail.com>
2024-06-18 00:46:37 -0400

    Remove space from copyright.

--- Shivers Randomizer/Properties/AssemblyInfo.cs
+++ Shivers Randomizer/Properties/AssemblyInfo.cs
@@ -10,7 +10,7 @@ using System.Windows;
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
 [assembly: AssemblyProduct("Shivers Randomizer")]
-[assembly: AssemblyCopyright("Copyright ©  2024")]
+[assembly: AssemblyCopyright("Copyright © 2024")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
